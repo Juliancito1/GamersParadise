de617c165c7ab8b83ee49c75bcbe278d8ad56617
Julian <julianser@hotmail.com.ar>
2024-06-20 01:26:37 -0300

    Se creo la estructura inicial y se instalaron las dependencias

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import AppRouter from './router/AppRouter'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import './App.css'
+
+function App() {
+  
+  return (
+    <section className='mainSection'>
+      <AppRouter/>
+    </section>
+  )
+}
+
+export default App
+
